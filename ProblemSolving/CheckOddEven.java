4443e747596b655cadcb329d015bfe1e33985909
Manish Royan <manishroyanofficial@outlook.com>
2024-05-29 13:54:45 +0545

    Check Odd Even
    Check if input number is Odd or Even

---
+++ ProblemSolving/CheckOddEven.java
@@ -0,0 +1,18 @@
+/* Check if the given number is Odd or Even */
+
+import java.util.Scanner;
+public class CheclOddEven {
+	public static void main(String[] args) {
+		int n;
+		System.out.println("Enter any number");
+		
+		Scanner input = new Scanner(System.in);
+		n = input.nextInt();
+		`
+		if(n%2==0) {
+			System.out.print(n + " is Even Number");
+		} else {
+			System.out.print(n +" is Odd number");
+		}
+	}
+}
