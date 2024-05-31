0e1b0f927026e3893c950778f1702877fe206284
Manish Royan <manishroyanofficial@outlook.com>
2024-05-31 21:31:57 +0545

    Count Digit of given number
    This program counts digits of given numbers by user.

--- ProblemSolving/CountDigit.java
+++ ProblemSolving/CountDigit.java
@@ -4,12 +4,13 @@ public class CountDigit {
     public static void main (String[] args) {
         
         //Variable decleartion
-        int n, count = 0; //we assign count = 0, so when n/10, thget incremented with '1'
+        int n, hold, count = 0; //we assign count = 0, so when n/value get incremented with '1'
         System.out.print("Enter any nubmer ");
         
         //User input
         Scanner input = new Scanner(System.in);
         n = input.nextInt();
+        hold = n;
         
         //logic implementation
         while(n>0) //the loop only works if our given is grester than '0;
@@ -19,7 +20,6 @@ public class CountDigit {
         }
         
         //Display result
-        System.out.print("No. of Digits: " + count);
+        System.out.print("No. of Digits for "+ hold +": " + count);
     }
-}
-                                
+}                                
