d14a51667565b70e872dd4eafcf54b641a21a748
Manish Royan <manishroyanofficial@outlook.com>
2024-06-11 12:44:22 +0545

    Java Stdin and Stdout - II

---
+++ HackerRank-Practice/Java-Stdin-and-Stdout_II/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt(); 
+
+        double d = scan.nextDouble(); //reads the next double from the input and assigns it to the variable 'd'.
+        
+        scan.nextLine(); //reads the remainder of the current line, including any leading whitespace characters, and discards it. This is necessary because 'nextInt()' and 'nextDouble()' do not consume the newline character after the number input, which can interfere with the subsequent 'nextLine()' method call.
+        
+        String s = scan.nextLine();  //scanner reads until next line occur
+        
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
