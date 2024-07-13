005dfceb367f21eb19a7ffe1ab03e77ab61a153f
Manish Royan <manishroyanofficial@outlook.com>
2024-07-13 20:17:09 +0545

    Update Solution.java

--- HackerRank-Practice/Java-Stdin-and-Stdout_II/Solution.java
+++ HackerRank-Practice/Java-Stdin-and-Stdout_II/Solution.java
@@ -10,7 +10,9 @@ public class Solution {
         
         scan.nextLine(); //reads the remainder of the current line, including any leading whitespace characters, and discards it. This is necessary because 'nextInt()' and 'nextDouble()' do not consume the newline character after the number input, which can interfere with the subsequent 'nextLine()' method call.
         
-        String s = scan.nextLine();  //scanner reads until next line occur
+        // String s = scan.next(); //'next()' will only reads until space occurs
+
+        String s = scan.nextLine();  //scanner 'nextLine()' reads until next line occur
         
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
